0b01cb7f99d0a1a5d8533654a7911c4f6b9008ac
nyka <nykanano@gmail.com>
2021-07-08 00:06:30 +0700

    - Update fragment_movie.xml

--- app/src/main/java/com/nyka/primedb/ui/MovieFragment.kt
+++ app/src/main/java/com/nyka/primedb/ui/MovieFragment.kt
@@ -14,6 +14,7 @@ class MovieFragment:Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
         return inflater.inflate(R.layout.fragment_movie,container,false)
     }
 
