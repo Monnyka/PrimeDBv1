d3ddb6585f9ac36a32769fe1f2c9724e1d1ae9e7
monnyka.pin <nykanano@gmail.com>
2022-08-23 21:32:11 +0700

    -Fix bug cannot build

--- app/src/main/java/com/nyka/primedb/ui/MovieViewModelProviderFactory.kt
+++ app/src/main/java/com/nyka/primedb/ui/MovieViewModelProviderFactory.kt
@@ -10,7 +10,7 @@ class MovieViewModelProviderFactory(
     val app: Application,
     private val movieRepository: MovieRepository
 ) : ViewModelProvider.Factory {
-        override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        override fun <T : ViewModel> create(modelClass: Class<T>): T {
         return MovieViewModel(app as MovieApplication, movieRepository) as T
     }
 }
